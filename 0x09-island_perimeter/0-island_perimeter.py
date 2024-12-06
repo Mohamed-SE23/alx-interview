7bb290f37339dd6d4bb0fce03f9be4ebcaf4f7d7
Mohamed Alamen <alamenmohamed536@gmail.com>
2024-12-06 05:28:27 +0200

    fixing island perimter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -17,7 +17,8 @@ def island_perimeter(grid):
         grid (list of list of int): A rectangular grid where:
                                     - 0 represents water.
                                     - 1 represents land.
-                                    - Cells are connected horizontally/vertically.
+                                    - Cells are connected
+                                      horizontally/vertically.
 
     Returns:
         int: The perimeter of the island.
@@ -25,7 +26,7 @@ def island_perimeter(grid):
     Assumptions:
                 - The grid is completely surrounded by water.
                 - There is only one island (or nothing).
-                - The island does not have "lakes" (water enclosed by land).
+                - The island does not have "lakes".
     """
     rows = len(grid)
     cols = len(grid[0])
@@ -35,19 +36,14 @@ def island_perimeter(grid):
         for c in range(cols):
             if grid[r][c] == 1:
                 cell_perimeter = 4
-
                 if r > 0 and grid[r - 1][c] == 1:
                     cell_perimeter -= 1
-
                 if r < rows - 1 and grid[r + 1][c] == 1:
                     cell_perimeter -= 1
-                        
                 if c > 0 and grid[r][c - 1] == 1:
                     cell_perimeter -= 1
-                        
                 if c < cols - 1 and grid[r][c + 1] == 1:
                     cell_perimeter -= 1
-
                 perimeter += cell_perimeter
 
     return perimeter
